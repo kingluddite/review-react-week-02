706f88177feca62216a519f0ed7c6f011bed6af2
Philip Howley <howley.phil@gmail.com>
2023-01-14 14:38:48 +0000

    Add object destructing

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 // let's learn about destructuring
+<<<<<<< Updated upstream
 console.log('hello world')
+=======
+// console.log('hello world')
+
+// object destructuring
+const car = {
+  model: 'ferrari',
+  numOfDoors: 2,
+  year: 1959
+}
+
+// const model = car.model;
+// console.log(model);
+
+const {model: superModel, numOfDoors, year} = car;
+console.log(superModel)
+console.log(numOfDoors)
+console.log(year)
+
+
+
+
+// const person = ['joe', 20];
+// // const name = person[0];
+// // const age = person[1];
+// // const [name, age] = person;
+// const [age, name] = person;
+
+// console.log(name, age);
+>>>>>>> Stashed changes
