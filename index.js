e76cb775e4f5512188331b9727d7f0e1bd12d53d
Philip Howley <howley.phil@gmail.com>
2023-01-14 14:28:54 +0000

    review array destructuring

---
+++ index.js
@@ -0,0 +1,10 @@
+// let's learn about destructuring
+// console.log('hello world')
+
+const person = ['joe', 20];
+// const name = person[0];
+// const age = person[1];
+// const [name, age] = person;
+const [age, name] = person;
+
+console.log(name, age);
